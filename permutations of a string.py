59e46322e73f7b5e7153598a1e799204b53e34d3
Mohammad Khalatbari <khalatbari.computer@gmail.com>
2022-12-05 18:19:45 +0200

    Update permutations of a string.py

--- permutations of a string.py
+++ permutations of a string.py
@@ -1,6 +1,4 @@
 result = []
-
-
 def permute(data, i, length):
     if i == length:
         result.append(''.join(data))
@@ -10,14 +8,10 @@ def permute(data, i, length):
             data[i], data[j] = data[j], data[i]
             permute(data, i + 1, length)
             data[i], data[j] = data[j], data[i]
-
+            
 ini_str = input('Give a string whose permutations you want to print:\n')
 permute(list(ini_str), 0, len(ini_str))
 result.sort()
 # Printing result
 print('Permutations of the string '+ini_str+' are:', end=' ')
-last = ''
-for i in result:
-    last = last + i +', '
-last = last[:-2] + '.'
-print(last)
+print(result)
